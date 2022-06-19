02a280ff0be40aaf260ed7549df7a6b6c78b214e
Stanley Godspower <godspowerstanley@gmail.com>
2022-06-19 14:14:45 +0100

    added models

--- I4G0081741DG/blog/views.py
+++ I4G0081741DG/blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, Stanley from the other side of the world!")
